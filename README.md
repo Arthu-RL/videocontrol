c923ba95d82bc21efe214a78a3dbe26cb228a1f4
ArthurRL <arthur.lima@mettainnovations.com.br>
2024-09-12 22:53:41 -0300

    Added: downsize.py

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # videocontrol
+
+Repo for video manipulation
+
+## Rodar códigos
+
+Concat videos
+
+```bash
+python concat_videos.py --videof-path="/path/to/your/video1.mp4" --videos-path="/path/to/your/video2.mp4" --outdir="./outputs"
+```
+
+Cut video
+
+```bash
+python cut_video.py --video-path="/path/to/your/video.mp4" ---start-time=10 --end-time=50
+```
+
+Downsize vídeo, lower FPS, smaller file size
+
+```bash
+python downsize.py -v /path/to/your/video.mp4 -o ./output_dir -s 0.5 --fps 30.0
+```
---
+++ downsize.py
@@ -0,0 +1,79 @@
+import cv2
+import numpy as np
+import argparse
+import os
+import logging
+from tqdm import tqdm
+
+logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
+
+parser: argparse.ArgumentParser = argparse.ArgumentParser()
+parser.add_argument("-v", "--video_path", dest="video_path", help="Caminho do vídeo", type=str, default='')
+parser.add_argument("-o", "--outdir", type=str, default='./outputs')
+parser.add_argument("-s", "--scale_resolution", dest="scale_resolution", help="Escala para diminuir, ou, aumentar resolução do vídeo", type=float, default=0.5)
+parser.add_argument("--fps", dest="fps",  help="FPS dop vídeo de saída", type=float)
+
+args: argparse.Namespace = parser.parse_args()
+
+cap: cv2.VideoCapture = cv2.VideoCapture(args.video_path)
+
+if not cap.isOpened():
+    logging.error(f"Error: Unable to open video file {args.video_path}")
+    exit(1)
+
+width: int = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height: int = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fps: float = float(cap.get(cv2.CAP_PROP_FPS))
+total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+logging.info(f"Video capture:")
+logging.info(f"width: {width}, height: {height}, fps: {fps}, total_frames: {total_frames}")
+
+
+resized_width: int = int(width * args.scale_resolution)
+resized_height: int = int(height * args.scale_resolution)
+resized_fps: float = args.fps
+
+logging.info(f"Video output:")
+logging.info(f"resized_width: {resized_width}, resized_height: {resized_height}, resized_fps: {resized_fps}")
+
+
+filename: str = os.path.basename(args.video_path)
+extension: str = "."+os.path.splitext(filename)[1]
+output_file: str = filename.replace(extension, "_down_sized"+extension)
+output_file = os.path.join(args.outdir, output_file)
+
+fourcc: int = cv2.VideoWriter.fourcc(*"mp4v")
+
+writer: cv2.VideoWriter = cv2.VideoWriter(output_file, cv2.CAP_ANY, fourcc, resized_fps, (resized_width, resized_height))
+
+if not writer.isOpened():
+    logging.error(f"Error: Unable to open the video writer for output file {output_file}")
+    cap.release()
+    exit(1)
+
+if not os.path.isdir(args.outdir):
+    os.makedirs(os.path.basename(args.outdir), exist_ok=True)
+
+
+progress_bar = tqdm(total=total_frames, desc="Processing frames", unit="frame")
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+    
+    resized_frame: cv2.Mat = cv2.resize(frame, (resized_width, resized_height))
+
+    writer.write(resized_frame)
+
+    progress_bar.update(1)
+
+
+cap.release()
+writer.release()
+
+progress_bar.close()
+
+logging.info(f"Video saved at: {output_file}")
